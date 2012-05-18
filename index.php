f2e457c4ef39b096b3ffa932395898cd4561fdc0
James Johnson <James.Johnson@excellentingenuity.com>
2012-05-18 10:21:29 -0400

    initial commit
    Signed-off-by: James Johnson <James.Johnson@excellentingenuity.com>

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+
+<!-- Page designed and coded by James Johnson of Excellent InGenuity LLC, 2012 under contract for ERF Marketing -->
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, intial-scale=1.0">
+<title>Griffin Property Solutions</title>
+
+<link rel="shortcut icon" href="img/fav_icon.png" />
+
+<link rel="stylesheet" media="all" href="css/bootstrap.css">
+<link rel="stylesheet" media="all" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" media="screen" href="css/phone.css">
+<link rel="stylesheet" media="screen and (min-width: 330px) and (max-width: 590px)" href="css/phone-hz.css"> 
+<link rel="stylesheet" media="screen and (min-width: 600px) and (max-width: 800px)" href="css/tablet.css">
+<link rel="stylesheet" media="screen and (min-width: 801px) and (max-width: 1280px)" href="css/desktop.css">
+<link rel="stylesheet" media="screen and (min-width: 1280px)" href="css/widescreen.css">
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+
+<!--javascript -->
+
+
+
+</head>
+<body>
+
+
+
+</body>
+</html>
